46c102cfcffcd56ebb6a03878fc537134834922a
benjamin <2000beni770@gmail.com>
2024-05-15 20:33:40 +0300

    up

--- src/main.ts
+++ src/main.ts
@@ -22,9 +22,10 @@ class Floor {
 class Elevator {
     public divElement: HTMLDivElement;
     public elevatorImg:HTMLImageElement;
-    public destination: number = 0;
+    public curentDestinationFloor: number = 0;
     public active: boolean = false;
-    public finishTim: number = 0; 
+    public finishTime: number = 0; 
+    private  audio = new Audio('music/ding.mp3');
 
     constructor(elvNum: number) {
         this.divElement = document.createElement('div');
@@ -33,7 +34,10 @@ class Elevator {
         this.elevatorImg = document.createElement('img');
         this.elevatorImg.src = 'images/elv.png';
         this.divElement.appendChild(this.elevatorImg);
+
+       
     }
+
 
     private speedCalcul(origin: number, targetFloor: number){
         return Math.abs(origin - targetFloor) * 0.5;
@@ -45,15 +49,48 @@ class Elevator {
     }
 
 
+    public UpDestination(targetFloor){
+        let originDestinationFloor = this.curentDestinationFloor;
+        this.curentDestinationFloor = targetFloor; 
+        return originDestinationFloor;
+    }
+
+    private gling() {
+        setTimeout(() => {
+            this.audio.play();
+        }, this.finishTime - Date.now()- 2000);
+    }
+    
     public move(origin: number, targetFloor: number) {
         let speed = this.speedCalcul(origin, targetFloor);
         let target = this.targetCalcul(targetFloor);
 
 
         this.divElement.style.transition = `transform ${speed}s ease`;
-        this.divElement.style.transform = `translateY(${target}px)`; 
+        this.divElement.style.transform = `translateY(${target}px)`;
+
+        this.gling()
     }
 
+
+    public floorDistance(newFloor:number){
+        return Math.abs(this.curentDestinationFloor - newFloor);
+    }
+
+
+    public TimeUpdate(){
+        if (this.finishTime < Date.now()){
+            this.finishTime = Date.now();
+        }
+    }
+
+
+    public TimeAdd(targetFloor: number){
+        this.TimeUpdate();
+        let addTime = this.floorDistance(targetFloor)*0.5 + 2;
+        this.finishTime += addTime*1000;
+        return  this.finishTime - addTime*1000;
+    }
 }
 
 
@@ -63,7 +100,6 @@ class ElevatorShaft {
     public divElement: HTMLDivElement;
     public elvArr: Elevator[] = [];
     public orderQueue: number[] = [];
-    // private elvAvailable = true
 
 
     constructor(elvNum: number){
@@ -77,39 +113,57 @@ class ElevatorShaft {
         }
     }
 
-    // private selectElv(targetFloor: number){
-       
-    //     return theElev;
-    // }
+
+    private selectElv(targetFloor: number){
+        let minTime: number = Infinity;
+        let res: number = 0;
+    
+        for(let i = 0; i < this.elvArr.length; i++){
+            
+            this.elvArr[i].TimeUpdate();
+            let time = (this.elvArr[i].floorDistance(targetFloor)*0.5)*1000 + (this.elvArr[i].finishTime - Date.now());
+
+            console.log(i ,":  time" ,time)
+            if (time < minTime){
+                minTime = time
+                res = i;
+            }
+        }
+        console.log("res" ,this.elvArr.length)
+        return res;
+    }
+
+    
+
+    private moveTiming(elv: Elevator, originFinishTime){
+
+        if((originFinishTime <= Date.now())){      
+            return 0;
+        }
+        return originFinishTime - Date.now();
+    }
+
+
+
+    private sendElv(elv: Elevator, targetFloor: number){
+        let originFinishTime =  elv.TimeAdd(targetFloor);
+        let originDestinationFloor = elv.UpDestination(targetFloor)
+
+        let waite = this.moveTiming(elv, originFinishTime)
+        setTimeout(() => {
+            elv.move(originDestinationFloor, targetFloor);
+        },waite);
+
+    }
 
 
     public ElvOrder(targetFloor: number){
-        let theElev: Elevator = this.elvArr[0]; 
-        let minTim: number = this.elvArr[0].finishTim + Math.abs(theElev.destination - targetFloor)*0.5;
-        this.elvArr.forEach(elev => {
-            let elvTim = theElev.finishTim + Math.abs(elev.destination - targetFloor)*0.5;
-            if (elvTim <= minTim){
-                theElev = elev;
-                minTim = elvTim;
-                }   
-        });
-
-        
-        let origin = theElev.destination;
-        theElev.destination = targetFloor;
-
-        let time = Math.abs(targetFloor - theElev.destination)*0.5
-        theElev.finishTim =+ Math.abs(targetFloor - theElev.destination)*0.5;
-
-
-
-        setTimeout(() => {
-            theElev.move(origin, targetFloor) 
-        }, theElev.finishTim + 2); 
-
-        
+        let select: number = this.selectElv(targetFloor) //select
+        this.sendElv(this.elvArr[select], targetFloor) //send
     }
 }
+
+
 
 class FloorColumn {
     public divElement: HTMLDivElement;
@@ -122,7 +176,6 @@ class FloorColumn {
             this.divElement.appendChild(floor.divElement);
         }
     }
-
 }
 
 
